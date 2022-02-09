76676e3d39d2867555f10c8b3bed87c9fc62f118
Gurtej736l <gurtej154@gmail.com>
2022-02-09 12:33:04 +1030

    update comments in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,16 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  //console.error('putDb not implemented');
   console.log("PUT to the database");
 
-  // Created a connection to the database database and version we want to use.
   const textDB = await openDB("jate", 1);
 
-  // Created a new transaction and specify the database and data privileges.
   const tx = textDB.transaction("jate", "readwrite");
   console.log("aaa");
-  // Open up the desired object store.
   const store = tx.objectStore("jate");
   console.log("bb");
   const objects = await store.getAll();
@@ -35,32 +31,22 @@ export const putDb = async (content) => {
 
   const request = await store.put(contenttoWrite);
 
-  // Use the .put() method on the store and pass in the content.
-  // const request = store.put({id: id, value:content});
-
-  // Get confirmation of the request.
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  //console.error('getDb not implemented');
   console.log("GET from the database Test");
   console.log("before opendb");
-  //creating connection to datav=base and version we want to use
   const textDB = await openDB("jate", 1);
   console.log("before transaction");
-  //creating a new transaction and specify the db and data privileges
   const tx = textDB.transaction("jate", "readonly");
   console.log("after transaction");
-  //open up the desired object store
   const store = tx.objectStore("jate");
 
-  // Used the .getAll() method to get all data in the database.
   const request = store.getAll();
 
-  // Get confirmation of the request.
   const result = await request;
   if (result.length > 0) {
     console.log("result.value", result);
@@ -69,9 +55,6 @@ export const getDb = async () => {
   } else {
     return null;
   }
-
-  // console.log('result.value', result);
-  // return result;
 };
 
 initdb();
