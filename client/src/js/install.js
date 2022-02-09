2de2fae1212af4faf01b2126839427f4a0f015dd
Gurtej736l <gurtej154@gmail.com>
2022-02-09 12:37:29 +1030

    update install.js with formation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,14 +4,11 @@ const butInstall = document.getElementById("buttonInstall");
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   debugger;
-  // Store the triggered events
   window.deferredPrompt = event;
 
-  // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   debugger;
   const promptEvent = window.deferredPrompt;
@@ -20,17 +17,13 @@ butInstall.addEventListener("click", async () => {
     return;
   }
 
-  // Show prompt
   promptEvent.prompt();
 
-  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
 
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  // Clear prompt
   window.deferredPrompt = null;
 });
