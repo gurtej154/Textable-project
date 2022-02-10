22e6bc521cd32d43b1d5231d441e9f6284420d4c
Gurtej736l <gurtej154@gmail.com>
2022-02-10 11:17:00 +1030

    add WorkboxPlugin into line 31 in webpack.config.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -26,6 +27,8 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+
+      new WorkboxPlugin.GenerateSW(),
 
       new WebpackPwaManifest({
         fingerprints: false,
